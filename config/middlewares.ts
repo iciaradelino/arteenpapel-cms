aba4d6c9dcc4a762eac8c2f3852a1647ab25544a
Iciar <iciaradelinoordax@gmail.com>
2025-08-20 17:28:51 +0200

    CORS configuration update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,15 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://arteenpapel.vercel.app', 'http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'Cache-Control', 'Pragma', 'Expires'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
